fc4c68512c66ca81754a7f90a4773e6668bc07bf
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2022-09-24 09:21:59 +0200

    Update split_c_network.ps1

--- split_c_network.ps1
+++ split_c_network.ps1
@@ -7,8 +7,6 @@ $ips = @{}
 
 $digits_as_int =[System.Convert]::ToInt32( $DigtisToSpend, 2 )
 
-$digits_as_int
-
 for ($i = 0; $i -le 255; $i++) {
     $ips[$digits_as_int -band $i ] += @("$NetworkAddress.$i")
 }
