b2018c740d8c94c092d2ed0cc2ce99ec41e05345
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2022-10-14 10:41:20 +0200

    Add files via upload

--- schemes.ps1
+++ schemes.ps1
@@ -1,17 +1,29 @@
 ﻿Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
+function actual_scheme_id {
+    (powercfg /getactivescheme) -match "\w+-\w+-\w+-\w+-\w+" > $null
+    $Matches[0]
+}
+
+
+function actual_scheme_name {
+    (powercfg /getactivescheme) -match "\(.+\)" > $null
+    $Matches[0] -replace "\(|\)",""
+}
+
+
+
+
 $form = [System.Windows.Forms.Form]::new()
 $form.Text = "Power-Setter"
 $form.Size = [System.Drawing.Size]::new(500,200)
 
 $label = [System.Windows.Forms.Label]::new()
-$label.Text = $( powercfg /getactivescheme )
+$label.Text = actual_scheme_name
 $label.Width = 300
 $label.Height = 150
 $label.Location = [System.Drawing.Point]::new(50,100)
-
-
 
 $combo = [System.Windows.Forms.ComboBox]::new()
 $combo.AutoSize = $true
@@ -22,20 +34,18 @@ $combo.add_SelectedIndexChanged({
     switch ( $selection ) {
         {$_ -eq 0} { 
             powercfg /setactive scheme_max
-            $label.Text = $( powercfg /getactivescheme )
+            $label.Text = actual_scheme_name
         }
         {$_ -eq 1} {
             powercfg /setactive scheme_balanced
-            $label.Text = $( powercfg /getactivescheme )
+            $label.Text = actual_scheme_name
         }
         {$_ -eq 2} {
             powercfg /setactive scheme_min
-            $label.Text = $( powercfg /getactivescheme )
+            $label.Text = actual_scheme_name
         }
     }
 })
-
-
 
 $form.Controls.AddRange(@($combo, $label))
 $form.ShowDialog()
