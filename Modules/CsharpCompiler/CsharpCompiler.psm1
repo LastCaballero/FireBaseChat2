67c20290c60c5681494dddae5c1e47a72642af72
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2023-04-24 17:39:10 +0200

    Add files via upload

--- Modules/CsharpCompiler/CsharpCompiler.psm1
+++ Modules/CsharpCompiler/CsharpCompiler.psm1
@@ -2,14 +2,12 @@
 
 function Compile-Csharp {
     param(
-        [string]$FileName,
-        [string]$ExeName
+        [string]$FileName
     )
     $FileToCompile = Get-Item $FileName
     $CodeProvider = [Microsoft.CSharp.CSharpCodeProvider]::new()
     $Compiler = $CodeProvider.CreateCompiler()
     $CompilerOptions = [System.CodeDom.Compiler.CompilerParameters]::new()
     $CompilerOptions.GenerateExecutable = $true
-    $CompilerOptions.OutputAssembly = $PWD + "\" + $ExeName
     $Compiler.CompileAssemblyFromFile( $CompilerOptions, $FileToCompile )
 }
