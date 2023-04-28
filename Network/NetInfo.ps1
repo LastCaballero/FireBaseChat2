631ab605ee60c05e76a4ea71a1136b13299adc2b
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2023-04-28 09:13:52 +0200

    Add files via upload

--- Network/NetInfo.ps1
+++ Network/NetInfo.ps1
@@ -2,10 +2,10 @@ $LocalIP = (Get-NetIPAddress | ? PrefixOrigin -eq "Dhcp").IPAddress
 $WebIp = (Invoke-WebRequest "https://api.ipify.org").Content
 $LocalGateWay = (Get-NetRoute -DestinationPrefix 0.0.0.0/0).NextHop
 
-$info = [System.Management.Automation.PSCustomObject]@{
+$Info = [System.Management.Automation.PSCustomObject]@{
     "Local-IP:" = $LocalIP
     "Web-IP:" = $WebIp
     "Local GateWay:" = $LocalGateWay
 }
 
-$info
+$Info
