fb5a47febe6d70cf0e4ee8baa2125979bb4bfecf
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2023-05-05 14:24:22 +0200

    Create VolksbankParser.ps1

---
+++ Banking/VolksbankParser.ps1
@@ -0,0 +1,26 @@
+Param(
+    $Bankbewegungen = "Volksbank_online_banking.csv"
+)
+
+
+$Csv = Import-Csv -Delimiter ";" $Bankbewegungen
+$Csv | ForEach-Object { $_.Betrag = [double]::Parse( $_.Betrag ) }
+
+class BankBewegung{
+    $Datum
+    $Betrag
+    BankBewegung($datum, $betrag){
+        $this.Datum = [datetime]::Parse($datum)
+        $this.Betrag = $betrag
+    }
+}
+
+$Zahlungsbeteiligte = $Csv | Group-Object -AsHashTable "Name Zahlungsbeteiligter"
+
+$Zahlungsbeteiligte.Keys | ForEach-Object {
+    "`n"
+    $_
+    $Zahlungsbeteiligte[$_] | ForEach-Object {
+        [BankBewegung]::new($_.Buchungstag, $_.Betrag)
+    } | Sort-Object -Property Datum
+}
