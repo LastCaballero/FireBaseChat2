67dd060f392b1316dabdd4022d71ce2dfe21723e
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2023-07-16 19:31:02 +0200

    Update LinuxTools.psm1

--- Modules/LinuxTools/LinuxTools.psm1
+++ Modules/LinuxTools/LinuxTools.psm1
@@ -1,11 +1,20 @@
 function Num-Lines {
+    param(
+        [string]$file = $null
+    )
     Begin {
         $lines = [System.Collections.ArrayList]::new()
     }
     Process {
         $lines.Add( $_ ) 1>$null
+        
     }
     End{
+        if( $file -ne $null -and (Test-Path $File) ) {
+            Get-Content $file | ForEach-Object {
+                $lines.Add( $_ ) 1>$null
+            }    
+        }
         $line = 1
         $formatter = $lines.Count.ToString().Length
         $lines | ForEach-Object {
