a5f48f4ed52a875fd05375545ce1361c7d34fa12
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2023-07-22 20:25:53 +0200

    Update scanner.ps1

--- scanner.ps1
+++ scanner.ps1
@@ -1,33 +1,23 @@
-
-[CmdletBinding()]
 param (
     [Parameter()]
-    $Target = "127.0.0.1",
-    [Int[]]$PortList 
+    $Target = "localhost",
+    [Int[]]$Ports = (4000..4004)+22+33
 )
 
-
-class Sniper{
-    [System.Threading.Tasks.Task]$TaskTcp
-    [System.Net.Sockets.TcpClient]$Client
-    [string]$Target
-    [int]$Port
-    Sniper([string]$Target, [int]$Port){
-        $this.Client = [System.Net.Sockets.TcpClient]::new()
-        $this.Target = $Target
-        $this.Port = $Port
-    }
-
-    [void]Connect(){
-        $this.TaskTcp = $this.Client.ConnectAsync($this.Target, $this.Port)
-    }
-    [bool]GetResult(){
-        return $this.TaskTcp.IsCompleted -and -not $this.TaskTcp.IsFaulted 
-    }
+$Ports | ForEach-Object {
+    Start-Job -Name Scanner -ArgumentList $Target, $_ {
+        try {
+            $socket = [System.Net.Sockets.TcpClient]::new()
+            $socket.Connect( $args[0], $args[1] )
+            "{0,-10}`t{1}" -f $args[1].ToString(), "open"
+            $socket.Dispose()
+        }catch {
+            "{0,-10}`t{1}" -f $args[1].ToString(), "closed"
+        } 
+    } 1>$null
 }
 
 
-$sniper = [Sniper]::new("localhost", 4000)
-$sniper.Connect()
-[System.Threading.Thread]::Sleep(1000)
-$sniper.GetResult()
+"`n`nScanreport for ${Target}:"
+( Receive-Job -Wait Scanner* ) | Sort-Object
+
