bd96d78e563876889e36caa041ad736841032fe9
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2023-07-22 22:41:23 +0200

    Update scanner.ps1

--- scanner.ps1
+++ scanner.ps1
@@ -1,23 +1,16 @@
 param (
     [Parameter()]
     $Target = "localhost",
-    [Int[]]$Ports = (4000..4004)+22+33
+    [Int[]]$Ports = (4000..4004)
 )
 
 $Ports | ForEach-Object {
     Start-Job -Name Scanner -ArgumentList $Target, $_ {
-        try {
-            $socket = [System.Net.Sockets.TcpClient]::new()
-            $socket.Connect( $args[0], $args[1] )
-            "{0,-10}`t{1}" -f $args[1].ToString(), "open"
-            $socket.Dispose()
-        }catch {
-            "{0,-10}`t{1}" -f $args[1].ToString(), "closed"
-        } 
-    } 1>$null
-}
+       $result = Test-NetConnection -ComputerName $args[0] -Port $args[1] 3>$null
+       $result 
+    }
+} 1>$null
 
 
 "`n`nScanreport for ${Target}:"
-( Receive-Job -Wait Scanner* ) | Sort-Object
-
+Receive-Job -Wait Scanner* | Sort-Object RemotePort | Format-Table
