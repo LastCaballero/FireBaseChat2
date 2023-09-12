3a332bb6423eb9bd4a7ee78bfe303e14dd3dc17e
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2023-09-12 21:04:46 +0200

    Update Nettools.psm1

--- Modules/Nettools/Nettools.psm1
+++ Modules/Nettools/Nettools.psm1
@@ -1,43 +1,14 @@
-function Get-MyIp(){
-    [System.Net.Dns]::GetHostAddresses( [System.Net.Dns]::GetHostName() )[1]
-}
-function Get-HostPart(){
-    param( $Ip )
-    ($Ip -split "\.")[ 0 .. 2 ] -join "."
-}
 
-function Get-NetworkNames(){
-    $MyIp = (Get-MyIp).ToString()
-    $HostPart = Get-HostPart -Ip $MyIp
-    $List = 1 .. 254 | ForEach-Object { "${HostPart}.$_" }
-    $List | ForEach-Object {
-        Start-Job -ArgumentList $_ {
-            $ip = $args[0]
-            try {
-                [System.Net.Dns]::GetHostByAddress( $ip )
-            } catch {}
-        }      
-    }
+$MyIp = [System.Net.Dns]::GetHostAddresses( [System.Net.Dns]::GetHostName() )[1].ToString()
+$HostPart = ($MyIp -split "\.")[ 0 .. 2 ] -join "." 
+$Ips = 1 .. 254 | ForEach-Object { "${HostPart}.$_" }
+
+function Get-KnownNetworkNames(){
+    $Tasks = $Ips | ForEach-Object { try { [System.Net.Dns]::GetHostEntryAsync( $_ ) } catch {} }
+    ( $Tasks | Where-Object { $_.Result -ne $null } ).Result
 }
 
 function Ping-Around(){
-    $MyIp = (Get-MyIp).ToString()
-    $HostPart = Get-HostPart -Ip $MyIp
-    $List = 1 .. 254 | ForEach-Object { "${HostPart}.$_" }
-    $Jobs = $List | ForEach-Object {
-        Start-job -ArgumentList $_ {
-            $Target = $args[0]
-            try {
-                [System.Net.NetworkInformation.Ping]::new().Send( $Target, 1 )      
-            } catch {}
-            
-        }
-        Write-Progress -Activity "${HostPart}.$_" -PercentComplete $_ / 255
-        
-    } 
-    $Jobs | Receive-Job -Wait | Select-Object Address, Status |
-        Where-Object { $_.Status -eq "Success" } |  Format-Table -AutoSize
+    $Tasks = $Ips | ForEach-Object { [System.Net.NetworkInformation.Ping]::new().SendPingAsync($_) }
+    $Tasks.Result | Where-Object { $_.Status -eq "Success" } | Format-Table
 }
-
-Get-NetworkNames
-
