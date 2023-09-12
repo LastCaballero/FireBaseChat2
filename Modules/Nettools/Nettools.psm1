fcfb8bb80be5732c8cc93100fd758408ca1bd3cb
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2023-09-12 23:35:58 +0200

    Update Nettools.psm1

--- Modules/Nettools/Nettools.psm1
+++ Modules/Nettools/Nettools.psm1
@@ -1,17 +1,20 @@
+function Set-Vars(){
+    $Global:MyIp = [System.Net.Dns]::GetHostAddresses( [System.Net.Dns]::GetHostName() )[1].ToString()
+    $Global:HostPart = ($MyIp -split "\.")[ 0 .. 2 ] -join "." 
+    $Global:Ips = 1 .. 254 | ForEach-Object { "${HostPart}.$_" }
+}
+
 
 function Get-KnownNetworkNames(){
-    $MyIp = [System.Net.Dns]::GetHostAddresses( [System.Net.Dns]::GetHostName() )[1].ToString()
-    $HostPart = ($MyIp -split "\.")[ 0 .. 2 ] -join "." 
-    $Ips = 1 .. 254 | ForEach-Object { "${HostPart}.$_" }
+    Set-Vars
     $Tasks = $Ips | ForEach-Object { try { [System.Net.Dns]::GetHostEntryAsync( $_ ) } catch {} }
     ( $Tasks | Where-Object { $_.Result -ne $null } ).Result
 }
 
 function Ping-Around(){
-    $MyIp = [System.Net.Dns]::GetHostAddresses( [System.Net.Dns]::GetHostName() )[1].ToString()
-    $HostPart = ($MyIp -split "\.")[ 0 .. 2 ] -join "." 
-    $Ips = 1 .. 254 | ForEach-Object { "${HostPart}.$_" }
+    Set-Vars
     $Tasks = $Ips | ForEach-Object { [System.Net.NetworkInformation.Ping]::new().SendPingAsync($_) }
     $Tasks.Result | Where-Object { $_.Status -eq "Success" } | Format-Table
 }
+
 
